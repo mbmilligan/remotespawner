a9b96d7ec847f439d38ca938673bd4575e2fa933
dolsen <dolsen@umn.edu>
2015-08-21 11:34:40 -0500

    Modified remotespawner to add (1) explicit key, and (2) remote sudo

--- remotespawner/remotespawner.py
+++ remotespawner/remotespawner.py
@@ -30,8 +30,8 @@ def setup_ssh_tunnel(port, user, server, keyfile):
 def execute(self, channel, command, process_user):
     """Execute command and get remote PID"""
 
-    # command = command + '& pid=$!; echo PID=$pid'
-    command = 'sudo -nH -u {username} -s "pid_exec {command} &"'.format(username=process_user, command=command)
+    command = command + '& pid=$!; echo PID=$pid'
+    #command = 'sudo -nH -u {username} -s "pid_exec {command} "'.format(username=process_user, command=command)
     stdin, stdout, stderr = channel.exec_command(command)
     self.log.error(stdout.readline())
     pid = int(stdout.readline().replace("PID=", ""))
